d26f831066bf8da373526aea099d258d366a5085
RedBerserk <nosbit98@mail.ru>
2023-11-21 23:09:01 +0300

    feat: add some solutions5

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -107,6 +107,7 @@ function getLinearEquationRoot(a, b) {
  */
 function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
   throw new Error('Not implemented');
+  // return Math.atan2(y1, x1) - Math.atan2(y2, x2);
   // return (x1 * x2 + y1 * y2) / (Math.sqrt(x1**2 + y1**2) * Math.sqrt(x2**2 + y2**2));
 }
 
